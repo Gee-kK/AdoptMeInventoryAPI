c4657b6bc1755c5eaf37357e223805f268318b20
geek <rivassamuel320@gmai.com>
2026-04-26 21:58:45 -0300

    update

--- main.lua
+++ main.lua
@@ -3,4 +3,16 @@ local ClientData = Fsys("ClientData")
 
 local inventory = ClientData.get("inventory")
 
-print(inventory)
+local VERSION = "0.1"
+
+
+for _, item in pairs(inventory) do
+    print(item)
+end
+
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Adopt Me API",
+	Text = `v{VERSION}`,
+	Icon = ""
+})
